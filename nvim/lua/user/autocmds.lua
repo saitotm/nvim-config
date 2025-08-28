267006fcb762810cd216ae829a2f27ab1343ea12
saitotm <tomokazu.ml.tech@gmail.com>
2025-08-28 17:50:47 +0900

    style: Format autocmd configuration

--- nvim/lua/user/autocmds.lua
+++ nvim/lua/user/autocmds.lua
@@ -2,4 +2,9 @@ local aug = vim.api.nvim_create_augroup
 local au = vim.api.nvim_create_autocmd
 
 aug("UserBasics", { clear = true })
-au("TextYankPost", { group = "UserBasics", callback = function() vim.highlight.on_yank() end })
+au("TextYankPost", {
+	group = "UserBasics",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
