66c9db36899a04c9fafa81a13095f6a22e8f8c89
saitotm <tomokazu.ml.tech@gmail.com>
2025-08-29 18:36:24 +0900

    feat: Add <leader>e keymap to close oil.nvim buffer
    Configure <leader>e to close oil buffer using built-in action

--- nvim/lua/common/plugins/ui/oil.lua
+++ nvim/lua/common/plugins/ui/oil.lua
@@ -2,7 +2,11 @@ return {
 	"stevearc/oil.nvim",
 	---@module 'oil'
 	---@type oil.SetupOpts
-	opts = {},
+	opts = {
+		keymaps = {
+			["<leader>e"] = "actions.close", -- Oil の組み込みアクション
+		},
+	},
 	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
 	lazy = false,
 }
