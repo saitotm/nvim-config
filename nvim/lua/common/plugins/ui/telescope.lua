e9a1f64de1885ffad0882041dcf9415047f220f8
saitotm <tomokazu.ml.tech@gmail.com>
2025-08-29 20:00:07 +0900

    feat: Configure Telescope to include hidden files in search
    - Enable hidden files visibility with hidden = true
    - Ignore .gitignore rules with no_ignore = true
    - Exclude .git directory from search results

--- nvim/lua/common/plugins/ui/telescope.lua
+++ nvim/lua/common/plugins/ui/telescope.lua
@@ -2,4 +2,13 @@ return {
 	"nvim-telescope/telescope.nvim",
 	cmd = "Telescope",
 	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+		pickers = {
+			find_files = {
+				hidden = true,
+				no_ignore = true,
+				file_ignore_patterns = { "%.git/" },
+			},
+		},
+	},
 }
