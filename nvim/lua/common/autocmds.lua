60e9ba6adebbbcbb6a13c3dcaede2278524a8fbf
saitotm <tomokazu.ml.tech@gmail.com>
2025-08-29 13:12:50 +0900

    refactor: Centralize LSP keymaps and prevent duplicate server setup
    Move LSP keymaps from nvim-lspconfig to autocmds.lua using LspAttach event
    to ensure consistent keymap configuration. Remove manual LSP server setup
    from nvim-lspconfig to prevent duplicate LSP client initialization.

--- nvim/lua/common/autocmds.lua
+++ nvim/lua/common/autocmds.lua
@@ -2,9 +2,26 @@ local aug = vim.api.nvim_create_augroup
 local au = vim.api.nvim_create_autocmd
 
 aug("UserBasics", { clear = true })
+
 au("TextYankPost", {
 	group = "UserBasics",
 	callback = function()
 		vim.highlight.on_yank()
+	end,
+})
+
+au("LspAttach", {
+	group = "UserBasics",
+	callback = function(args)
+		local telescope = require("telescope.builtin")
+		local map = function(mode, lhs, rhs, desc)
+			vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+		end
+
+		map("n", "gd", telescope.lsp_definitions, "Definitions (Telescope)")
+		map("n", "gD", vim.lsp.buf.declaration, "Go to Declaration")
+		map("n", "gi", telescope.lsp_implementations, "Implementations (Telescope)")
+		map("n", "gr", telescope.lsp_references, "References (Telescope)")
+		map("n", "K", vim.lsp.buf.hover, "Hover")
 	end,
 })
--- nvim/lua/common/plugins/lsp/nvim-lspconfig.lua
+++ nvim/lua/common/plugins/lsp/nvim-lspconfig.lua
@@ -1,12 +1,3 @@
 return {
 	"neovim/nvim-lspconfig",
-	config = function()
-		local lsp = require("lspconfig")
-		local caps = require("cmp_nvim_lsp").default_capabilities()
-
-		lsp.lua_ls.setup({ capabilities = caps })
-		lsp.pyright.setup({ capabilities = caps })
-		lsp.ts_ls.setup({ capabilities = caps })
-		lsp.rust_analyzer.setup({ capabilities = caps })
-	end,
 }
