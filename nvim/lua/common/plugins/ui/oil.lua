8bb20e461f7823a0219e57c7b8e495d4ff9657fc
saitotm <tomokazu.ml.tech@gmail.com>
2025-08-30 06:44:56 +0900

    feat: Configure oil.nvim to show hidden files by default

--- nvim/lua/common/plugins/ui/oil.lua
+++ nvim/lua/common/plugins/ui/oil.lua
@@ -6,6 +6,9 @@ return {
 		keymaps = {
 			["<leader>e"] = "actions.close", -- Oil の組み込みアクション
 		},
+		view_options = {
+			show_hidden = true,
+		},
 	},
 	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
 	lazy = false,
